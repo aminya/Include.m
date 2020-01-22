6983ef2bd6e25350273b42f0484271e4974b6519
aminya <aminyahyaabadi74@gmail.com>
2020-01-21 18:28:55 -0600

    addpather

--- addpather.m
+++ addpather.m
@@ -1,3 +1,43 @@
+%% AddPather.m
+% Copyright (c) 2020 Amin Yahyaabadi - aminyahyaabadi74@gmail.com
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function addpather(method, varargin)
+    % addpather function
+    %
+    % # Arguments:
+    % addpather(method::String, [folders::Array{String}/Cell{Char}])
+    %
+    %  - method: can be "all","all_exclude","specific","GUI_all","GUI_specific"
+    %  - folders (optional):
+    %   - if method is "all_exclude": pass the folder names that should be excluded
+    %   - if method is "specific": pass the folder names that should be included
+    %
+    %  Folders specified in the 2nd argument can have a relative as well as absolute path.
+    % 
+    % # Example run_addpather
+    % Choose the method, and run the script.
+    % ```matlab
+    % % runs the function:
+    % addpather("all");
+    %
+    % clear method;
+    % ```
+    %
+    % Pass a 2nd output to include/exclude specific folders if you chose "all_exclude" or "specificFolders"
+    % ```matlab
+    % % runs the function:
+    % addpather("specific", ["src", "examples"]); % or addpather("specific", {'src', 'examples'});
+    %
+    % clear method;
+    % ```
+
+
+    switch method
+
+end
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [folders] = dirFolder(parentDir)
     % get folder names in a directory
 
