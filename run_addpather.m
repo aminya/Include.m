8d24660de43b17d36d4b88da44c262c84e724221
aminya <aminyahyaabadi74@gmail.com>
2020-01-21 18:34:47 -0600

    Create run_addpather.m

---
+++ run_addpather.m
@@ -0,0 +1,10 @@
+%% Add folders to your path
+
+% choose the method, and run the script.
+% pass a 2nd output to include/exclude specific folders if you chose "all_exclude" or "specificFolders"
+method="all";
+
+% runs the function:
+addpather(method);
+
+clear method;
